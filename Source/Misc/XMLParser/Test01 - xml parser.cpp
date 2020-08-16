c4beb9f85e0dbbd299ce93d4ea16df8016d5df9c
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-08-17 01:30:41 +0200

    Testing XML parser

--- Source/Misc/XMLParser/Test01 - xml parser.cpp
+++ Source/Misc/XMLParser/Test01 - xml parser.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+#include "XMLParser.h"
+
 int main()
 {
+	try
+	{
+		XMLParser parser("Ressources/scene.xml");
+
+		TreeNode scene = parser.ParseFile();
+
+		scene.ls_r();
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "Exception : " << e.what() << std::endl;
+	}
+
 	std::cin.get();
 }

3cf77388ccd642ba6d98df406ba8b22f34e34309
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-08-17 01:29:52 +0200

    Added TreeNode class

--- Source/Misc/XMLParser/TreeNode.cpp
+++ Source/Misc/XMLParser/TreeNode.cpp
@@ -1,6 +1,48 @@
 #include "TreeNode.h"
 
-TreeNode::TreeNode(const char * name, const char * geometry) :
-	m_Name(name),
-	m_Geometry(geometry)
+#include <iostream>
+
+const int indentScale = 2;
+
+TreeNode::TreeNode(const TreeNode& other) :
+	m_Name(other.m_Name),
+	m_Transform(other.m_Transform),
+	m_Geometry(other.m_Geometry),
+	m_Children(other.m_Children)
 {}
+
+TreeNode::TreeNode(TreeNode&& other) :
+	m_Name(std::move(other.m_Name)),
+	m_Transform(std::move(other.m_Transform)),
+	m_Geometry(std::move(other.m_Geometry)),
+	m_Children(std::move(other.m_Children))
+{}
+
+TreeNode& TreeNode::AddChild(const TreeNode& child)
+{
+	m_Children.push_back(child);
+
+	return m_Children[m_Children.size() - 1];
+}
+
+TreeNode& TreeNode::AddChild(TreeNode&& child)
+{
+	m_Children.push_back(std::move(child));
+
+	return m_Children[m_Children.size() - 1];
+}
+
+void TreeNode::ls_r(size_t indent) const
+{
+	std::string _indent(indentScale * indent++, ' ');
+
+	std::cout << _indent << m_Name;
+
+	if (!m_Geometry.empty())
+		std::cout << " - " << m_Geometry;
+
+	std::cout << std::endl;
+
+	for (const TreeNode& e : m_Children)
+		e.ls_r(indent);
+}
--- Source/Misc/XMLParser/TreeNode.h
+++ Source/Misc/XMLParser/TreeNode.h
@@ -7,11 +7,39 @@ class TreeNode
 {
 private:
 	std::string m_Name;
-	std::string m_Geometry;
 	std::string m_Transform;
+	std::string m_Geometry;
 
 	std::vector<TreeNode> m_Children;
 
 public:
-	TreeNode(const char* name, const char* geometry = nullptr);
+	TreeNode() = default;
+
+	TreeNode(const TreeNode& other);
+
+	TreeNode(TreeNode&& other);
+
+	TreeNode& AddChild(const TreeNode& child);
+
+	TreeNode& AddChild(TreeNode&& child);
+
+	template<typename ...Args>
+	TreeNode& AddChildEmplace(Args&& ...args)
+	{
+		m_Children.emplace_back(std::forward<Args>(args)...);
+
+		return m_Children[m_Children.size() - 1];
+	}
+
+	void ls_r(size_t indent = 0) const;
+
+	std::string& Name()      { return m_Name; }
+	std::string& Transform() { return m_Transform; }
+	std::string& Geometry()  { return m_Geometry; }
+
+	const std::string& Name() const      { return m_Name; }
+	const std::string& Transform() const { return m_Transform; }
+	const std::string& Geometry() const  { return m_Geometry; }
+
+	const std::vector<TreeNode>& Children() const { return m_Children; }
 };

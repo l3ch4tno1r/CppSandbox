887b3211ae79bd4798cec3d6b2c842dd7fd13dbc
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-08-17 01:25:56 +0200

    Testing RegEx

---
+++ Source/RegEx/Test01 - regex.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <regex>
+
+#define SEPARATOR(X) std::cout << "------- " << X << " -------" << std::endl
+
+int main()
+{
+	std::string str = "<Transformation>1.0 0. 0. 0. 0.0 1.0 0.0 5.0  0. 0. 1. 0. 0. 0. 0. 1.0</Transformation>";
+
+	std::regex  transformRegex("<Transformation>((-?\\d+\\.\\d* *){16})</Transformation>");
+	std::smatch matches;
+
+	SEPARATOR("Search and get matches");
+	std::regex_search(str, matches, transformRegex);
+	SEPARATOR("End Search and get matches");
+	SEPARATOR("Search");
+	std::regex_search(str, transformRegex);
+	SEPARATOR("End Search");
+
+	std::cout << matches.size() << std::endl;
+
+	for (auto& m : matches)
+		std::cout << m.str() << std::endl;
+
+	/*
+	std::stringstream sstr(matches[1].str());
+
+	float tab[16];
+
+	for (size_t i = 0; i < 16; ++i)
+		sstr >> tab[i];
+
+	for (size_t i = 0; i < 4; ++i)
+	{
+		for (size_t j = 0; j < 4; ++j)
+			std::cout << tab[4 * i + j] << '\t';
+
+		std::cout << std::endl;
+	}
+	*/
+
+	std::cin.get();
+}

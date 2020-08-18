31b458dfc2a060b22e081b443bd89c19e3cea921
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-08-19 01:41:54 +0200

    WIP

--- Source/Misc/XMLParser/Scene.h
+++ Source/Misc/XMLParser/Scene.h
@@ -14,6 +14,24 @@ public:
 	BaseNode(const char* name, const Transform3D<float>& transform);
 
 	virtual ~BaseNode() {}
+
+	const std::string& Name() const             { return m_Name; }
+	const Transform3D<float>& Transform() const { return m_Transform; }
 };
 
+class GeometryNode : public BaseNode
+{
+private:
+	std::string m_Geometry;
 
+public:
+	GeometryNode(const char* name, const char* geometry, const Transform3D<float>& transform) :
+		BaseNode(name, transform),
+		m_Geometry(geometry)
+	{}
+
+	const std::string& Geometry() const { return m_Geometry; }
+};
+
+class GroupNode : public BaseNode
+{};

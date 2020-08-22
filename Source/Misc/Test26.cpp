c2b1b2c566a3864f4ef5a03269de17b3c84af218
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-08-22 03:59:57 +0200

    Update Test26.cpp

--- Source/Misc/Test26.cpp
+++ Source/Misc/Test26.cpp
@@ -63,6 +63,15 @@ public:
 	const std::string& Name() const { return m_Name; }
 };
 
+template <int... Is>
+struct index {};
+
+template <int N, int... Is>
+struct gen_seq : gen_seq<N - 1, N - 1, Is...> {};
+
+template <int... Is>
+struct gen_seq<0, Is...> : index<Is...> {};
+
 int main()
 {
 	/*

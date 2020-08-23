993fa2586eb5b1c9088f2dc147633af595322483
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-08-23 23:17:45 +0200

    Update _MemTracker.cpp

--- Source/Memory/_MemTracker.cpp
+++ Source/Memory/_MemTracker.cpp
@@ -1,4 +1,4 @@
-#define MEM_TRACKING 0
+#define MEM_TRACKING 1
 
 #if MEM_TRACKING == 1
 #include <mutex>
@@ -6,7 +6,7 @@
 
 #define DEBUG_MEM 1
 
-#if DEBUG_MEM == 0
+#if DEBUG_MEM == 1
 #define LOG(X) X
 #else
 #define LOG(X)

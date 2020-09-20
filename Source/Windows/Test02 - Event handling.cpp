913440268a15d5501dfc02e3406d89b149cd7a5b
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-09-21 00:21:16 +0200

    Event handling WIP

--- Source/Windows/Test02 - Event handling.cpp
+++ Source/Windows/Test02 - Event handling.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 #include <functional>
+#include <thread>
+#include <chrono>
+#include <atomic>
+
+using namespace std::literals::chrono_literals;
 
 #define NOMINMAX
 #include <Windows.h>
 
 #define PRINTVAL(X) std::cout << #X << " = " << X << std::endl
+
+std::atomic<bool> run = true;
+
+void Thread()
+{
+	while (run)
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+
+		std::cout << "Working" << std::endl;
+
+		std::this_thread::sleep_until(start + 1s);
+	}
+}
 
 int main()
 {
@@ -13,9 +32,9 @@ int main()
 	DWORD        cNumRead;
 	INPUT_RECORD irInBuf[128];
 
-	SetConsoleMode(hStdin, ENABLE_EXTENDED_FLAGS | ENABLE_MOUSE_INPUT | ENABLE_WINDOW_INPUT);
+	//std::thread thread(Thread);
 
-	bool run = true;
+	SetConsoleMode(hStdin, ENABLE_EXTENDED_FLAGS | ENABLE_MOUSE_INPUT | ENABLE_WINDOW_INPUT);
 
 	struct KeyState
 	{
@@ -25,20 +44,36 @@ int main()
 		bool _NA         : 3;
 		bool KeyOldState : 1;
 		bool KeyNewState : 1;
-	} keys[256];
+	} keys[256], mouse[5];
 
 	std::memset(keys, 0, 256 * sizeof(KeyState));
+	std::memset(mouse, 0, 5 * sizeof(KeyState));
 
 	using KeyBoardAction = std::function<void(const KeyState&)>;
+	using MouseAction    = std::function<void(const KeyState&, int, int)>;
 
 	KeyBoardAction keyactions[256];
 
-	keyactions[VK_ESCAPE] = [&run](const KeyState&) { run = false; };
+	keyactions[VK_ESCAPE] = [](const KeyState&) { run = false; };
 	keyactions[VK_SPACE]  = [](const KeyState& key)
 	{
 		if(key.KeyPressed)
 			std::cout << "Jump !" << std::endl;
 	};
+
+	MouseAction mouseactions[5];
+
+	for (int i = 0; i < 5; ++i)
+	{
+		mouseactions[i] = [i](const KeyState& mstate, int x, int y)
+		{
+			if(mstate.KeyPressed)
+				std::cout << i << " pressed : (" << x << ", " << y << ')' << std::endl;
+
+			if (mstate.KeyReleased)
+				std::cout << i << " released : (" << x << ", " << y << ')' << std::endl;
+		};
+	}
 
 	while (run)
 	{
@@ -57,9 +92,9 @@ int main()
 				KeyBoardAction& keyaction = keyactions[record.Event.KeyEvent.wVirtualKeyCode];
 
 				key.KeyNewState = record.Event.KeyEvent.bKeyDown;
-				key.KeyHeld     = key.KeyNewState;
-				key.KeyPressed  = !key.KeyOldState && key.KeyNewState;
-				key.KeyReleased = key.KeyOldState && !key.KeyNewState;
+				key.KeyHeld     =  key.KeyNewState;
+				key.KeyPressed  = !key.KeyOldState &&  key.KeyNewState;
+				key.KeyReleased =  key.KeyOldState && !key.KeyNewState;
 
 				if(keyaction)
 					keyaction(key);
@@ -68,9 +103,45 @@ int main()
 
 				break;
 			}
+			case MOUSE_EVENT:
+			{
+				switch (record.Event.MouseEvent.dwEventFlags)
+				{
+				case MOUSE_MOVED:
+				{
+					break;
+				}
+				case 0:
+				{
+					for (int i = 0; i < 5; ++i)
+					{
+						mouse[i].KeyNewState = (record.Event.MouseEvent.dwButtonState & (1 << i)) > 0;
+						mouse[i].KeyHeld     =  mouse[i].KeyNewState;
+						mouse[i].KeyPressed  = !mouse[i].KeyOldState &&  mouse[i].KeyNewState;
+						mouse[i].KeyReleased =  mouse[i].KeyOldState && !mouse[i].KeyNewState;
+
+						int x = record.Event.MouseEvent.dwMousePosition.X;
+						int y = record.Event.MouseEvent.dwMousePosition.Y;
+
+						if (mouseactions[i])
+							mouseactions[i](mouse[i], x, y);
+
+						mouse[i].KeyOldState = mouse[i].KeyNewState;
+					}
+
+					break;
+				}
+				default:
+					break;
+				}
+
+				break;
+			}
 			default:
 				break;
 			}
 		}
 	}
+
+	//thread.join();
 }

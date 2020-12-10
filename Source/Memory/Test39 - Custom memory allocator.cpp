050425c805a2de9a21cdf497004283d8eaee9065
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-12-11 00:27:59 +0100

    Memory management tests

--- Source/Memory/Test39 - Custom memory allocator.cpp
+++ Source/Memory/Test39 - Custom memory allocator.cpp
@@ -1,20 +1,47 @@
 #include <iostream>
 #include <memory>
-#include <thread>
+#include <vector>
+
+#include <Utilities/Source/InstanceCounter.h>
+
+#define SEPARATOR(X) std::cout << "----- " << X << " -----" << std::endl;
+
+class Test : public Counter<Test>
+{
+public:
+	Test() { std::cout << "Hello world " << this->Id() << std::endl; }
+	~Test() { std::cout << "Goodbye world " << this->Id() << std::endl; }
+};
 
 int main()
 {
-	{
-		std::unique_ptr<int> ptr = std::make_unique<int>();
-
-		*ptr = 12;
-
-		std::cout << *ptr << std::endl;
-	}
-
 	try
 	{
-		int* ptr = new int[0];
+		SEPARATOR(0);
+		{
+			std::unique_ptr<int> ptr = std::make_unique<int>();
+
+			*ptr = 12;
+
+			std::cout << *ptr << std::endl;
+		}
+
+		SEPARATOR(1);
+		{
+			Test* ptr = new Test[10];
+
+			delete[] ptr;
+		}
+
+		SEPARATOR(2);
+		{
+			std::vector<Test> vector;
+
+			vector.reserve(10);
+
+			for (int i = 0; i < vector.capacity(); i++)
+				vector.emplace_back();
+		}
 	}
 	catch (const std::exception& e)
 	{

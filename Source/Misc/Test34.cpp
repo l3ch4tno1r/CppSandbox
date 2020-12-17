f821f928ca817548b5d053dddb6ed29ae16419a4
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-12-17 23:45:24 +0100

    Minor commit

--- Source/Misc/Test34.cpp
+++ Source/Misc/Test34.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 
 #include <Utilities/Source/ConfigManager.h>
+#include <Utilities/Source/InstanceCounter.h>
+
+class Test : public Counter<Test>
+{
+public:
+	Test() { std::cout << "default ctor : " << this->Id() << std::endl; }
+
+	Test(const Test& other) :
+		c(other.c)
+	{
+		std::cout << "copy ctor : " << this->Id() << std::endl;
+	}
+
+	Test(Test&& other) :
+		c(other.c)
+	{
+		std::cout << "move ctor : " << this->Id() << std::endl;
+	}
+
+	char C() const { return c; }
+	char& C() { return c; }
+
+private:
+	char c;
+};
+
+std::istream& operator>>(std::istream& istream, Test& arg)
+{
+	istream >> arg.C();
+
+	return istream;
+}
 
 int main()
 {
@@ -8,9 +40,8 @@ int main()
 	{
 		auto& appsettings = LCNUtilities::ConfigManager::AppSettings();
 
-		char key = appsettings["goforward"];
-
-		std::cout << key << std::endl;
+		Test key1 = appsettings["goforward"];
+		Test key2 = appsettings["gobackward"];
 	}
 	catch (const std::exception& e)
 	{

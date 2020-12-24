821a6aaa3dbf196bed3ddd81fae16b3869edb938
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-12-24 01:20:15 +0100

    Entity like WIP

---
+++ Source/SQLite/SQLiteEntity/Expression.h
@@ -0,0 +1,488 @@
+#pragma once
+
+namespace LCN {
+	namespace SQLiteEntity {
+
+#pragma region Expression
+		////////////////////
+		//-- Expression --//
+		////////////////////
+
+		template<class E>
+		class Expression
+		{
+		public:
+			// Must override
+			inline size_t Size() const { return this->Derived().Size(); }
+
+			inline char operator[](size_t i) const { return this->Derived()[i]; }
+
+			// Methods
+			std::string ToString() const
+			{
+				std::string result(this->Size(), '\0');
+
+				for (size_t i = 0; i < this->Size(); ++i)
+					result[i] = (*this)[i];
+
+				return result;
+			}
+
+			const char* ToCString() const
+			{
+				char* result = new char[this->Size() + 1];
+
+				for (size_t i = 0; i < this->Size(); ++i)
+					result[i] = (*this)[i];
+
+				result[this->Size()] = '\0';
+
+				return result;
+			}
+
+			inline operator std::string() const { return this->ToString(); }
+
+			inline operator char*() const { return this->ToCString(); }
+
+		protected:
+			inline E& Derived() { return static_cast<E&>(*this); }
+			inline const E& Derived() const { return static_cast<const E&>(*this); }
+		};
+
+#pragma endregion
+
+#pragma region Variable
+		//////////////////
+		//-- Entities --//
+		//////////////////
+
+		template<class Derived>
+		class Variable : public Expression<Derived>
+		{};
+
+#pragma endregion
+
+#pragma region Predicate
+
+#pragma region Bases
+		///////////////////
+		//-- Predicate --//
+		///////////////////
+
+		template<class Derived>
+		class Predicate : public Expression<Derived>
+		{};
+
+		//////////////////
+		//-- Unary Op --//
+		//////////////////
+
+		template<class Derived, class E>
+		class UnaryPredicate : public Predicate<Derived>
+		{
+		public:
+			inline size_t Size() const { return 2 + Derived::OperatorName().size() + this->e.Size(); }
+
+			inline char operator[](size_t i) const
+			{
+				size_t i_off = i;
+
+				const std::string& operatorname = Derived::OperatorName();
+
+				if (i_off < operatorname.size())
+					return operatorname[i_off];
+
+				i_off -= operatorname.size();
+
+				if (0 == i_off)
+					return '(';
+
+				--i_off;
+
+				if (i_off < this->e.Size())
+					return this->e[i_off];
+
+				i_off -= this->e.Size();
+
+				if (0 == i_off)
+					return ')';
+
+				throw std::out_of_range("Index out of range.");
+			}
+
+		protected:
+			const E& e;
+
+			UnaryPredicate(const E& e) :
+				e(e)
+			{}
+		};
+
+		///////////////////
+		//-- Binary Op --//
+		///////////////////
+
+		template<class Derived, class EL, class ER>
+		class BinaryPredicate : public Predicate<Derived>
+		{
+		public:
+			inline size_t Size() const { return 2 + Derived::OperatorName().size() + this->el.Size() + this->er.Size(); }
+
+			inline char operator[](size_t i) const
+			{
+				if (0 == i)
+					return '(';
+
+				if (this->Size() - 1 == i)
+					return ')';
+
+				size_t offseted = i - 1;
+
+				if (offseted < this->el.Size())
+					return this->el[offseted];
+
+				offseted -= this->el.Size();
+
+				const std::string& operatorname = Derived::OperatorName();
+
+				if (offseted < operatorname.size())
+					return operatorname[offseted];
+
+				offseted -= operatorname.size();
+
+				if (offseted < this->er.Size())
+					return this->er[offseted];
+
+				throw std::out_of_range("Index out of range.");
+			}
+
+		protected:
+			const EL& el;
+			const ER& er;
+
+			BinaryPredicate(const EL& el, const ER& er) :
+				el(el),
+				er(er)
+			{}
+		};
+#pragma endregion
+
+#pragma region Comparison
+		////////////////////
+		//-- Comparison --//
+		////////////////////
+
+		template<class Derived, class EL, class ER>
+		class ComparisonStatement : public BinaryPredicate<Derived, EL, ER>
+		{
+		public:
+			using Base = BinaryPredicate<Derived, EL, ER>;
+
+		protected:
+			ComparisonStatement(const EL& el, const ER& er) :
+				Base(el, er)
+			{}
+		};
+
+		///////////////
+		//-- Equal --//
+		///////////////
+
+		template<class EL, class ER>
+		class EqualStatement : public ComparisonStatement<EqualStatement<EL, ER>, EL, ER>
+		{
+		public:
+			using Base = ComparisonStatement<EqualStatement<EL, ER>, EL, ER>;
+
+			static const std::string& OperatorName()
+			{
+				static std::string name(" = ");
+				return name;
+			}
+
+		private:
+			EqualStatement(const EL& el, const ER& er) :
+				Base(el, er)
+			{}
+
+			template<class _EL, class _ER>
+			friend EqualStatement<_EL, _ER> operator==(const Variable<_EL>&, const Variable<_ER>&);
+		};
+
+		template<class _EL, class _ER>
+		EqualStatement<_EL, _ER> operator==(const Variable<_EL>& el, const Variable<_ER>& er)
+		{
+			return EqualStatement<_EL, _ER>(static_cast<const _EL&>(el), static_cast<const _ER&>(er));
+		}
+
+		///////////////////
+		//-- Not Equal --//
+		///////////////////
+
+		template<class EL, class ER>
+		class NotEqualStatement : public ComparisonStatement<NotEqualStatement<EL, ER>, EL, ER>
+		{
+		public:
+			using Base = ComparisonStatement<NotEqualStatement<EL, ER>, EL, ER>;
+
+			static const std::string& OperatorName()
+			{
+				static std::string name(" <> ");
+				return name;
+			}
+
+		private:
+			NotEqualStatement(const EL& el, const ER& er) :
+				Base(el, er)
+			{}
+
+			template<class _EL, class _ER>
+			friend NotEqualStatement<_EL, _ER> operator!=(const Variable<_EL>&, const Variable<_ER>&);
+		};
+
+		template<class _EL, class _ER>
+		NotEqualStatement<_EL, _ER> operator!=(const Variable<_EL>& el, const Variable<_ER>& er)
+		{
+			return NotEqualStatement<_EL, _ER>(static_cast<const _EL&>(el), static_cast<const _ER&>(er));
+		}
+
+		/////////////////
+		//-- Greater --//
+		/////////////////
+
+		template<class EL, class ER>
+		class GreaterStatement : public ComparisonStatement<GreaterStatement<EL, ER>, EL, ER>
+		{
+		public:
+			using Base = ComparisonStatement<GreaterStatement<EL, ER>, EL, ER>;
+
+			static const std::string& OperatorName()
+			{
+				static std::string name(" > ");
+				return name;
+			}
+
+		private:
+			GreaterStatement(const EL& el, const ER& er) :
+				Base(el, er)
+			{}
+
+			template<class _EL, class _ER>
+			friend GreaterStatement<_EL, _ER> operator>(const Variable<_EL>&, const Variable<_ER>&);
+		};
+
+		template<class _EL, class _ER>
+		GreaterStatement<_EL, _ER> operator>(const Variable<_EL>& el, const Variable<_ER>& er)
+		{
+			return GreaterStatement<_EL, _ER>(static_cast<const _EL&>(el), static_cast<const _ER&>(er));
+		}
+
+		//////////////////////////
+		//-- Greater or equal --//
+		//////////////////////////
+
+		template<class EL, class ER>
+		class GreaterOrEqualStatement : public ComparisonStatement<GreaterOrEqualStatement<EL, ER>, EL, ER>
+		{
+		public:
+			using Base = ComparisonStatement<GreaterOrEqualStatement<EL, ER>, EL, ER>;
+
+			static const std::string& OperatorName()
+			{
+				static std::string name(" >= ");
+				return name;
+			}
+
+		private:
+			GreaterOrEqualStatement(const EL& el, const ER& er) :
+				Base(el, er)
+			{}
+
+			template<class _EL, class _ER>
+			friend GreaterOrEqualStatement<_EL, _ER> operator>=(const Variable<_EL>&, const Variable<_ER>&);
+		};
+
+		template<class _EL, class _ER>
+		GreaterOrEqualStatement<_EL, _ER> operator>=(const Variable<_EL>& el, const Variable<_ER>& er)
+		{
+			return GreaterOrEqualStatement<_EL, _ER>(static_cast<const _EL&>(el), static_cast<const _ER&>(er));
+		}
+
+		//////////////
+		//-- Less --//
+		//////////////
+
+		template<class EL, class ER>
+		class LessStatement : public ComparisonStatement<LessStatement<EL, ER>, EL, ER>
+		{
+		public:
+			using Base = ComparisonStatement<LessStatement<EL, ER>, EL, ER>;
+
+			static const std::string& OperatorName()
+			{
+				static std::string name(" < ");
+				return name;
+			}
+
+		private:
+			LessStatement(const EL& el, const ER& er) :
+				Base(el, er)
+			{}
+
+			template<class _EL, class _ER>
+			friend LessStatement<_EL, _ER> operator<(const Variable<_EL>&, const Variable<_ER>&);
+		};
+
+		template<class _EL, class _ER>
+		LessStatement<_EL, _ER> operator<(const Variable<_EL>& el, const Variable<_ER>& er)
+		{
+			return LessStatement<_EL, _ER>(static_cast<const _EL&>(el), static_cast<const _ER&>(er));
+		}
+
+		///////////////////////
+		//-- Less or equal --//
+		///////////////////////
+
+		template<class EL, class ER>
+		class LessOrEqualStatement : public ComparisonStatement<LessOrEqualStatement<EL, ER>, EL, ER>
+		{
+		public:
+			using Base = ComparisonStatement<LessOrEqualStatement<EL, ER>, EL, ER>;
+
+			static const std::string& OperatorName()
+			{
+				static std::string name(" <= ");
+				return name;
+			}
+
+		private:
+			LessOrEqualStatement(const EL& el, const ER& er) :
+				Base(el, er)
+			{}
+
+			template<class _EL, class _ER>
+			friend LessOrEqualStatement<_EL, _ER> operator<=(const Variable<_EL>&, const Variable<_ER>&);
+		};
+
+		template<class _EL, class _ER>
+		LessOrEqualStatement<_EL, _ER> operator<=(const Variable<_EL>& el, const Variable<_ER>& er)
+		{
+			return LessOrEqualStatement<_EL, _ER>(static_cast<const _EL&>(el), static_cast<const _ER&>(er));
+		}
+
+#pragma endregion
+
+#pragma region Logical
+		//////////////////////////
+		//-- Logical negation --//
+		//////////////////////////
+
+		template<class E>
+		class NotStatment : public UnaryPredicate<NotStatment<E>, E>
+		{
+		public:
+			using Base = UnaryPredicate<NotStatment<E>, E>;
+
+			static const std::string& OperatorName()
+			{
+				static std::string name("not ");
+				return name;
+			}
+
+		private:
+			NotStatment(const E& e) :
+				Base(e)
+			{}
+
+			template<class _E>
+			friend NotStatment<_E> operator!(const Predicate<_E>&);
+		};
+
+		template<class _E>
+		NotStatment<_E> operator!(const Predicate<_E>& e)
+		{
+			return NotStatment<_E>(static_cast<const _E&>(e));
+		}
+
+		/////////////////
+		//-- Logical --//
+		/////////////////
+
+		template<class Derived, class EL, class ER>
+		class LogicalStatement : public BinaryPredicate<Derived, EL, ER>
+		{
+		public:
+			using Base = BinaryPredicate<Derived, EL, ER>;
+
+		protected:
+			LogicalStatement(const EL& el, const ER& er) :
+				Base(el, er)
+			{}
+		};
+
+		/////////////////////
+		//-- Logical and --//
+		/////////////////////
+
+		template<class EL, class ER>
+		class AndStatement : public LogicalStatement<AndStatement<EL, ER>, EL, ER>
+		{
+		public:
+			using Base = LogicalStatement<AndStatement<EL, ER>, EL, ER>;
+
+			static const std::string& OperatorName()
+			{
+				static std::string name(" and ");
+				return name;
+			}
+
+		private:
+			AndStatement(const EL& el, const ER& er) :
+				Base(el, er)
+			{}
+
+			template<class _EL, class _ER>
+			friend AndStatement<_EL, _ER> operator&&(const Predicate<_EL>&, const Predicate<_ER>&);
+		};
+
+		template<class _EL, class _ER>
+		AndStatement<_EL, _ER> operator&&(const Predicate<_EL>& el, const Predicate<_ER>& er)
+		{
+			return AndStatement<_EL, _ER>(static_cast<const _EL&>(el), static_cast<const _ER&>(er));
+		}
+
+		////////////////////
+		//-- Logical or --//
+		////////////////////
+
+		template<class EL, class ER>
+		class OrStatement : public LogicalStatement<OrStatement<EL, ER>, EL, ER>
+		{
+		public:
+			using Base = LogicalStatement<OrStatement<EL, ER>, EL, ER>;
+
+			static const std::string& OperatorName()
+			{
+				static std::string name(" or ");
+				return name;
+			}
+
+		private:
+			OrStatement(const EL& el, const ER& er) :
+				Base(el, er)
+			{}
+
+			template<class _EL, class _ER>
+			friend OrStatement<_EL, _ER> operator||(const Predicate<_EL>&, const Predicate<_ER>&);
+		};
+
+		template<class _EL, class _ER>
+		OrStatement<_EL, _ER> operator||(const Predicate<_EL>& el, const Predicate<_ER>& er)
+		{
+			return OrStatement<_EL, _ER>(static_cast<const _EL&>(el), static_cast<const _ER&>(er));
+		}
+#pragma endregion
+
+#pragma endregion
+	}
+}
--- Source/SQLite/Test06.cpp
+++ Source/SQLite/Test06.cpp
@@ -1,46 +1,23 @@
 #include <iostream>
 #include <string>
+#include <functional>
 
 #include "Source/Memory/_MemTracker.h"
 
+#include "SQLiteEntity/Expression.h"
+
 using namespace std::literals::string_literals;
+using namespace LCN::SQLiteEntity;
 
 #define SEPARATOR(X) std::cout << "------- " << X << " -------" << std::endl
 
-#pragma region SQLiteExpression
+#pragma region Expression
 //////////////////////////
 //-- SQLiteExpression --//
 //////////////////////////
 
-template<class E>
-class SQLiteExpression
-{
-public:
-	// Must override
-	inline size_t Size() const { return this->Derived().Size(); }
-
-	inline char operator[](size_t i) const { return this->Derived()[i]; }
-
-	// Methods
-	std::string ToString() const
-	{
-		std::string result(this->Size(), '\0');
-
-		for (size_t i = 0; i < this->Size(); ++i)
-			result[i] = (*this)[i];
-
-		return result;
-	}
-
-	inline operator std::string() const { return this->ToString(); }
-
-protected:
-	inline E& Derived() { return static_cast<E&>(*this); }
-	inline const E& Derived() const { return static_cast<const E&>(*this); }
-};
-
 template<class Derived>
-std::ostream& operator<<(std::ostream& ostream, const SQLiteExpression<Derived>& expr)
+std::ostream& operator<<(std::ostream& ostream, const Expression<Derived>& expr)
 {
 	for (size_t i = 0; i < expr.Size(); ++i)
 		ostream << expr[i];
@@ -51,19 +28,12 @@ std::ostream& operator<<(std::ostream& ostream, const SQLiteExpression<Derived>&
 #pragma endregion
 
 #pragma region Entities
-//////////////////
-//-- Entities --//
-//////////////////
-
-template<class Derived>
-class SQLiteEntity : public SQLiteExpression<Derived>
-{};
 
 ///////////////
 //-- Tests --//
 ///////////////
 
-class QuickTest : public SQLiteEntity<QuickTest>
+class QuickTest : public Variable<QuickTest>
 {
 public:
 	inline size_t Size() const { return 4; }
@@ -76,344 +46,84 @@ public:
 	}
 };
 
-class Test : public SQLiteEntity<Test>
+template<class Obj>
+class Column : public Variable<Column<Obj>>
 {
 public:
-	Test(const char* name) :
+	Column(const char* name) :
 		m_Name(name)
 	{}
 
-	inline size_t Size() const { return m_Name.size(); }
+	inline size_t Size() const { return 1 + Obj::Name().size() + m_Name.size(); }
 
-	inline char operator[](size_t i) const { return m_Name[i]; }
+	inline char operator[](size_t i) const
+	{
+		size_t i_off = 0;
+
+		if (i < Obj::Name().size())
+			return Obj::Name()[i];
+
+		i -= Obj::Name().size();
+
+		if (0 == i)
+			return '.';
+
+		--i;
+
+		if(i < m_Name.size())
+			return m_Name[i];
+
+		throw std::out_of_range("Index out of range.");
+	}
 
 private:
 	std::string m_Name;
 };
 
-#define CREATE_TEST(X) Test X(#X)
+template<class C>
+class Entity;
 
-#pragma endregion
+#define CONSTRUCT_FIELD(X) X(*this, #X)
 
-#pragma region Predicate
-
-#pragma region Bases
-///////////////////
-//-- Predicate --//
-///////////////////
-
-template<class Derived>
-class Predicate : public SQLiteExpression<Derived>
-{};
-
-///////////////////
-//-- Binary Op --//
-///////////////////
-
-template<class Derived, class EL, class ER>
-class BinaryPredicate : public Predicate<Derived>
+class Contact
 {
 public:
-	inline size_t Size() const { return 2 + Derived::OperatorName().size() + this->el.Size() + this->er.Size(); }
+	const std::string& FirstName() const { return m_FirstName; }
+	std::string& FirstName() { return m_FirstName; }
 
-	inline char operator[](size_t i) const
-	{
-		if (0 == i)
-			return '(';
-
-		if (this->Size() - 1 == i)
-			return ')';
-
-		size_t offseted = i - 1;
-
-		if (offseted < this->el.Size())
-			return this->el[offseted];
-
-		offseted -= this->el.Size();
-
-		const std::string& operatorname = Derived::OperatorName();
-
-		if (offseted < operatorname.size())
-			return operatorname[offseted];
-
-		offseted -= operatorname.size();
-
-		if (offseted < this->er.Size())
-			return this->er[offseted];
-
-		throw std::out_of_range("Index out of range.");
-	}
-
-protected:
-	const EL& el;
-	const ER& er;
-
-	BinaryPredicate(const EL& el, const ER& er) :
-		el(el),
-		er(er)
-	{}
-};
-#pragma endregion
-
-#pragma region Comparison
-////////////////////
-//-- Comparison --//
-////////////////////
-
-template<class Derived, class EL, class ER>
-class ComparisonStatement : public BinaryPredicate<Derived, EL, ER>
-{
-public:
-	using Base = BinaryPredicate<Derived, EL, ER>;
-
-protected:
-	ComparisonStatement(const EL& el, const ER& er) :
-		Base(el, er)
-	{}
-};
-
-///////////////
-//-- Equal --//
-///////////////
-
-template<class EL, class ER>
-class EqualStatement : public ComparisonStatement<EqualStatement<EL, ER>, EL, ER>
-{
-public:
-	using Base = ComparisonStatement<EqualStatement<EL, ER>, EL, ER>;
-
-	static const std::string& OperatorName()
-	{
-		static std::string name(" = ");
-		return name;
-	}
+	const std::string& LastName() const { return m_LastName; }
+	std::string& LastName() { return m_LastName; }
 
 private:
-	EqualStatement(const EL& el, const ER& er) :
-		Base(el, er)
-	{}
-
-	template<class _EL, class _ER>
-	friend EqualStatement<_EL, _ER> operator==(const SQLiteEntity<_EL>&, const SQLiteEntity<_ER>&);
+	std::string m_FirstName;
+	std::string m_LastName;
 };
 
-template<class _EL, class _ER>
-EqualStatement<_EL, _ER> operator==(const SQLiteEntity<_EL>& el, const SQLiteEntity<_ER>& er)
-{
-	return EqualStatement<_EL, _ER>(static_cast<const _EL&>(el), static_cast<const _ER&>(er));
-}
-
-///////////////////
-//-- Not Equal --//
-///////////////////
-
-template<class EL, class ER>
-class NotEqualStatement : public ComparisonStatement<NotEqualStatement<EL, ER>, EL, ER>
+template<>
+class Entity<Contact>
 {
 public:
-	using Base = ComparisonStatement<NotEqualStatement<EL, ER>, EL, ER>;
+	using ColumnType = Column<Entity<Contact>>;
 
-	static const std::string& OperatorName()
+	static ColumnType FirstName;
+	static ColumnType LastName;
+
+	static const std::string& Name()
 	{
-		static std::string name(" <> ");
+		static std::string name = "Contact";
 		return name;
 	}
-
-private:
-	NotEqualStatement(const EL& el, const ER& er) :
-		Base(el, er)
-	{}
-
-	template<class _EL, class _ER>
-	friend NotEqualStatement<_EL, _ER> operator!=(const SQLiteEntity<_EL>&, const SQLiteEntity<_ER>&);
 };
 
-template<class _EL, class _ER>
-NotEqualStatement<_EL, _ER> operator!=(const SQLiteEntity<_EL>& el, const SQLiteEntity<_ER>& er)
-{
-	return NotEqualStatement<_EL, _ER>(static_cast<const _EL&>(el), static_cast<const _ER&>(er));
-}
+using eContact = Entity<Contact>;
 
-/////////////////
-//-- Greater --//
-/////////////////
-
-template<class EL, class ER>
-class GreaterStatement : public ComparisonStatement<GreaterStatement<EL, ER>, EL, ER>
-{
-public:
-	using Base = ComparisonStatement<GreaterStatement<EL, ER>, EL, ER>;
-
-	static const std::string& OperatorName()
-	{
-		static std::string name(" > ");
-		return name;
-	}
-
-private:
-	GreaterStatement(const EL& el, const ER& er) :
-		Base(el, er)
-	{}
-
-	template<class _EL, class _ER>
-	friend GreaterStatement<_EL, _ER> operator>(const SQLiteEntity<_EL>&, const SQLiteEntity<_ER>&);
-};
-
-template<class _EL, class _ER>
-GreaterStatement<_EL, _ER> operator>(const SQLiteEntity<_EL>& el, const SQLiteEntity<_ER>& er)
-{
-	return GreaterStatement<_EL, _ER>(static_cast<const _EL&>(el), static_cast<const _ER&>(er));
-}
-
-//////////////////////////
-//-- Greater or equal --//
-//////////////////////////
-
-template<class EL, class ER>
-class GreaterOrEqualStatement : public ComparisonStatement<GreaterOrEqualStatement<EL, ER>, EL, ER>
-{
-public:
-	using Base = ComparisonStatement<GreaterOrEqualStatement<EL, ER>, EL, ER>;
-
-	static const std::string& OperatorName()
-	{
-		static std::string name(" >= ");
-		return name;
-	}
-
-private:
-	GreaterOrEqualStatement(const EL& el, const ER& er) :
-		Base(el, er)
-	{}
-
-	template<class _EL, class _ER>
-	friend GreaterOrEqualStatement<_EL, _ER> operator>=(const SQLiteEntity<_EL>&, const SQLiteEntity<_ER>&);
-};
-
-template<class _EL, class _ER>
-GreaterOrEqualStatement<_EL, _ER> operator>=(const SQLiteEntity<_EL>& el, const SQLiteEntity<_ER>& er)
-{
-	return GreaterOrEqualStatement<_EL, _ER>(static_cast<const _EL&>(el), static_cast<const _ER&>(er));
-}
-
-//////////////
-//-- Less --//
-//////////////
-
-template<class EL, class ER>
-class LessStatement : public ComparisonStatement<LessStatement<EL, ER>, EL, ER>
-{
-public:
-	using Base = ComparisonStatement<LessStatement<EL, ER>, EL, ER>;
-
-	static const std::string& OperatorName()
-	{
-		static std::string name(" < ");
-		return name;
-	}
-
-private:
-	LessStatement(const EL& el, const ER& er) :
-		Base(el, er)
-	{}
-
-	template<class _EL, class _ER>
-	friend LessStatement<_EL, _ER> operator<(const SQLiteEntity<_EL>&, const SQLiteEntity<_ER>&);
-};
-
-template<class _EL, class _ER>
-LessStatement<_EL, _ER> operator<(const SQLiteEntity<_EL>& el, const SQLiteEntity<_ER>& er)
-{
-	return LessStatement<_EL, _ER>(static_cast<const _EL&>(el), static_cast<const _ER&>(er));
-}
-
-///////////////////////
-//-- Less or equal --//
-///////////////////////
-
-template<class EL, class ER>
-class LessOrEqualStatement : public ComparisonStatement<LessOrEqualStatement<EL, ER>, EL, ER>
-{
-public:
-	using Base = ComparisonStatement<LessOrEqualStatement<EL, ER>, EL, ER>;
-
-	static const std::string& OperatorName()
-	{
-		static std::string name(" <= ");
-		return name;
-	}
-
-private:
-	LessOrEqualStatement(const EL& el, const ER& er) :
-		Base(el, er)
-	{}
-
-	template<class _EL, class _ER>
-	friend LessOrEqualStatement<_EL, _ER> operator<=(const SQLiteEntity<_EL>&, const SQLiteEntity<_ER>&);
-};
-
-template<class _EL, class _ER>
-LessOrEqualStatement<_EL, _ER> operator<=(const SQLiteEntity<_EL>& el, const SQLiteEntity<_ER>& er)
-{
-	return LessOrEqualStatement<_EL, _ER>(static_cast<const _EL&>(el), static_cast<const _ER&>(er));
-}
-
-#pragma endregion
-
-#pragma region Logical
-/////////////////
-//-- Logical --//
-/////////////////
-
-template<class Derived, class EL, class ER>
-class LogicalStatement : public BinaryPredicate<Derived, EL, ER>
-{
-public:
-	using Base = BinaryPredicate<Derived, EL, ER>;
-
-protected:
-	LogicalStatement(const EL& el, const ER& er) :
-		Base(el, er)
-	{}
-};
-
-/////////////////////
-//-- Logical and --//
-/////////////////////
-
-template<class EL, class ER>
-class AndStatement : public LogicalStatement<AndStatement<EL, ER>, EL, ER>
-{
-public:
-	using Base = LogicalStatement<AndStatement<EL, ER>, EL, ER>;
-
-	static const std::string& OperatorName()
-	{
-		static std::string name(" and ");
-		return name;
-	}
-
-private:
-	AndStatement(const EL& el, const ER& er) :
-		Base(el, er)
-	{}
-
-	template<class _EL, class _ER>
-	friend AndStatement<_EL, _ER> operator&&(const Predicate<_EL>&, const Predicate<_ER>&);
-};
-
-template<class _EL, class _ER>
-AndStatement<_EL, _ER> operator&&(const Predicate<_EL>& el, const Predicate<_ER>& er)
-{
-	return AndStatement<_EL, _ER>(static_cast<const _EL&>(el), static_cast<const _ER&>(er));
-}
-#pragma endregion
+eContact::ColumnType eContact::FirstName("FirstName");
+eContact::ColumnType eContact::LastName("LastName");
 
 #pragma endregion
 
 int main()
 {
-	/*
 	SEPARATOR(1);
 	{
 		auto session = MemTracker::Get().BeginScopeBasedSession();
@@ -425,7 +135,7 @@ int main()
 		auto equals =
 			test1 == test2 &&
 			test2 >  test1 &&
-			test1 != test1 &&
+			test1 != test1 ||
 			test2 >= test3 &&
 			test3 <  test1 &&
 			test1 <= test2;
@@ -450,14 +160,14 @@ int main()
 		std::cout << (test1 >  test2) << std::endl;
 		std::cout << (test1 >= test2) << std::endl;
 	}
-	*/
 
+	/*
 	SEPARATOR(3);
 	{
 		auto session = MemTracker::Get().BeginScopeBasedSession();
 
-		CREATE_TEST(FirstName);
-		CREATE_TEST(LastName);
+		CREATE_FIELD(FirstName);
+		CREATE_FIELD(LastName);
 
 		std::cout << (FirstName == LastName) << std::endl;
 		std::cout << (FirstName != LastName) << std::endl;
@@ -471,8 +181,8 @@ int main()
 	{
 		auto session = MemTracker::Get().BeginScopeBasedSession();
 
-		CREATE_TEST(FirstName);
-		CREATE_TEST(LastName);
+		CREATE_FIELD(FirstName);
+		CREATE_FIELD(LastName);
 
 		std::cout << (FirstName == LastName) << std::endl;
 		std::cout << (FirstName != LastName) << std::endl;
@@ -480,6 +190,24 @@ int main()
 		std::cout << (FirstName <= LastName) << std::endl;
 		std::cout << (FirstName >  LastName) << std::endl;
 		std::cout << (FirstName >= LastName) << std::endl;
+	}
+
+	SEPARATOR(4);
+	{
+		CREATE_FIELD(Col1);
+		CREATE_FIELD(Col2);
+		CREATE_FIELD(Col3);
+
+		std::cout << (Col1 == Col2 && Col2 == Col3 && Col3 == Col1) << std::endl;
+		std::cout << (Col1 == Col2 && (Col2 == Col3 && Col3 == Col1)) << std::endl;
+		std::cout << (Col1 == Col2 || Col2 == Col3 && Col3 == Col1) << std::endl;
+		std::cout << (Col1 == Col2 && Col2 == Col3 || !(Col3 == Col1)) << std::endl;
+	}
+	*/
+
+	SEPARATOR(3);
+	{
+		std::cout << (eContact::FirstName > eContact::LastName) << std::endl;
 	}
 
 	std::cin.get();

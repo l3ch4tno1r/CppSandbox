7f8e3a909ad0ddece4f3a9e24b8570765aa92338
l3ch4tno1r <arnaud.granier34@gmail.com>
2021-01-22 00:37:50 +0100

    Testing Matrix block

--- Source/Math/Test11.cpp
+++ Source/Math/Test11.cpp
@@ -1,34 +1,58 @@
 #include <iostream>
+#include <iomanip>
 
-#include "LCN_Math/Source/Matrix/Matrix.h"
-#include "LCN_Math/Source/Matrix/MatrixBlock.h"
+#include <LCN_Math/Source/Matrix/Matrix.h>
+#include <LCN_Math/Source/Matrix/MatrixBlock.h>
+#include <LCN_Math/Source/Geometry/Vector.h>
+
+#define SEPARATOR(X) std::cout << "-------- " << X << " --------" << std::endl;
 
 int main()
 {
-	LCN::Matrix4f m = {
-		1, 0,  0, 1,
-		0, 0, -1, 2,
-		0, 1,  0, 3,
-		0, 0,  0, 1
-	};
+	SEPARATOR(1)
+	{
+		LCN::Matrix4f m = {
+			1, 0,  0, 1,
+			0, 0, -1, 2,
+			0, 1,  0, 3,
+			0, 0,  0, 1
+		};
 
-	LCN::MatrixBlock<LCN::Matrix4f, 0, 0, 3, 3> r(m);
-	LCN::MatrixBlock<LCN::Matrix4f, 0, 3, 3, 1> t(m);
+		LCN::Matrix<float, 4, 1> v = { 1, 1, 1 ,1 };
 
-	std::cout << m << std::endl;
-	std::cout << r << std::endl;
-	std::cout << t << std::endl;
+		const auto& ref = m;
 
-	t(1, 0) = 7;
+		LCN::MatrixBlock<LCN::Matrix4f, 3, 3> r(m, 0, 0);
+		LCN::MatrixBlock<LCN::Matrix4f, 3, 1> t(m, 0, 3);
 
-	std::cout << m << std::endl;
+		std::cout << m << std::endl;
+		std::cout << r << std::endl;
+		std::cout << t << std::endl;
 
-	//LCN::MatrixBlock<LCN::Matrix4f, 0, 0, 5, 1> test1(m);
-	//LCN::MatrixBlock<LCN::Matrix4f, 0, 0, 1, 5> test2(m);
-	//LCN::MatrixBlock<LCN::Matrix4f, 4, 0, 3, 3> test3(m);
-	//LCN::MatrixBlock<LCN::Matrix4f, 0, 4, 3, 3> test4(m);
-	//LCN::MatrixBlock<LCN::Matrix4f, 0, 3, 3, 2> test5(m);
-	//LCN::MatrixBlock<LCN::Matrix4f, 3, 0, 2, 3> test6(m);
+		t(1, 0) = 7;
+
+		std::cout << m << std::endl;
+
+		std::cout << m.Columns()[2] << std::endl;
+		std::cout << m.Rows()[1] << std::endl;
+
+		std::cout << m * v << std::endl;
+
+		//LCN::MatrixBlock<LCN::Matrix4f, 0, 0, 5, 1> test1(m);
+		//LCN::MatrixBlock<LCN::Matrix4f, 0, 0, 1, 5> test2(m);
+		//LCN::MatrixBlock<LCN::Matrix4f, 4, 0, 3, 3> test3(m);
+		//LCN::MatrixBlock<LCN::Matrix4f, 0, 4, 3, 3> test4(m);
+		//LCN::MatrixBlock<LCN::Matrix4f, 0, 3, 3, 2> test5(m);
+		//LCN::MatrixBlock<LCN::Matrix4f, 3, 0, 2, 3> test6(m);
+	}
+
+	SEPARATOR(2)
+	{
+		LCN::HVector3Df hv1 = { 1, 2, -1, 0 };
+		LCN::HVector3Df hv2 = { 3, 4, -1, 0 };
+
+		std::cout << (hv1.Vector() ^ hv2.Vector()) << std::endl;
+	}
 
 	std::cin.get();
 }

8938621d90f2238df00a04b937811c9562569a20
l3ch4tno1r <arnaud.granier34@gmail.com>
2021-01-23 17:09:30 +0100

    Benchmarking

--- Source/Math/Test12 - Benchmark.cpp
+++ Source/Math/Test12 - Benchmark.cpp
@@ -6,21 +6,31 @@
 #include <LCN_Math/Source/Matrix/Transpose.h>
 #include <LCN_Math/Source/Geometry/Transform.h>
 
+using namespace LCN::Benchmark;
+
 int main()
 {
-	const size_t iterations = 100000;
+	const size_t iterations = 200000;
 
 	LCN::Transform3Df T;
 
-	Benchmark::TimePerformance(iterations, [&]()
+	std::future<Session> session1 = TimePerformanceAsync(iterations, [&]()
 		{
-			auto iT = T.Invert();
+			auto iT = T.Inverse();
 		});
 
-	Benchmark::TimePerformance(iterations, [&]()
+	std::future<Session> session2 = TimePerformanceAsync(iterations, [&]()
 		{
 			auto iT = T.QuickInverse();
 		});
+
+	long long d1 = session1.get().Duration(DurationUnit::Milliseconds);
+	long long d2 = session2.get().Duration(DurationUnit::Milliseconds);
+
+	std::cout << "Session duration : " << d1 << "ms" << std::endl;
+	std::cout << "Session duration : " << d2 << "ms" << std::endl;
+
+	std::cout << "Gain : " << (double)d1 / (double)d2 << std::endl;
 
 	std::cin.get();
 }

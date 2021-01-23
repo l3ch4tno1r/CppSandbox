7847063672919dc344aa8d0efda260f760549c11
l3ch4tno1r <arnaud.granier34@gmail.com>
2021-01-23 01:53:47 +0100

    Testing Matrix2 utilities

--- Source/Math/Test11.cpp
+++ Source/Math/Test11.cpp
@@ -3,6 +3,8 @@
 
 #include <LCN_Math/Source/Matrix/Matrix.h>
 #include <LCN_Math/Source/Matrix/MatrixBlock.h>
+#include <LCN_Math/Source/Matrix/Transpose.h>
+
 #include <LCN_Math/Source/Geometry/Vector.h>
 
 #define SEPARATOR(X) std::cout << "-------- " << X << " --------" << std::endl;
@@ -38,20 +40,56 @@ int main()
 
 		std::cout << m * v << std::endl;
 
-		//LCN::MatrixBlock<LCN::Matrix4f, 0, 0, 5, 1> test1(m);
-		//LCN::MatrixBlock<LCN::Matrix4f, 0, 0, 1, 5> test2(m);
-		//LCN::MatrixBlock<LCN::Matrix4f, 4, 0, 3, 3> test3(m);
-		//LCN::MatrixBlock<LCN::Matrix4f, 0, 4, 3, 3> test4(m);
-		//LCN::MatrixBlock<LCN::Matrix4f, 0, 3, 3, 2> test5(m);
-		//LCN::MatrixBlock<LCN::Matrix4f, 3, 0, 2, 3> test6(m);
+		std::cout << ref.Columns()[0] << std::endl;
+		std::cout << ref.Rows()[1] << std::endl;
+		//LCN::ConstMatrixBlock<LCN::Matrix4f, 3, 3> cr(ref, 0, 0);
 	}
 
 	SEPARATOR(2)
 	{
 		LCN::HVector3Df hv1 = { 1, 2, -1, 0 };
 		LCN::HVector3Df hv2 = { 3, 4, -1, 0 };
+		LCN::HVector3Df hv3;
 
-		std::cout << (hv1.Vector() ^ hv2.Vector()) << std::endl;
+		hv3.Vector() = hv1.Vector() ^ hv2.Vector();
+
+		std::cout << std::endl;
+	}
+
+	SEPARATOR(3)
+	{
+		LCN::HVector3Df hv1 = { 1, 2, 3, 0 };
+		LCN::Transpose<LCN::HVector3Df> thv1(hv1);
+
+		std::cout << hv1 << std::endl;
+		std::cout << thv1 << std::endl;
+
+		std::cout << thv1 * hv1 << std::endl;
+		std::cout << hv1 * thv1 << std::endl;
+	}
+
+	SEPARATOR(4)
+	{
+		LCN::Matrix4f m = {
+			1, 0,  0, 1,
+			0, 0, -1, 2,
+			0, 1,  0, 3,
+			0, 0,  0, 1
+		};
+
+		LCN::MatrixBlock<LCN::Matrix4f, 3, 3> r(m, 0, 0);
+		LCN::MatrixBlock<LCN::Matrix4f, 3, 1> t(m, 0, 3);
+
+		LCN::Matrix4f im = LCN::Matrix4f::Identity();
+
+		LCN::MatrixBlock<LCN::Matrix4f, 3, 3> ir(im, 0, 0);
+		LCN::MatrixBlock<LCN::Matrix4f, 3, 1> it(im, 0, 3);
+
+		ir = r.Transpose();
+		it = -(ir * t);
+
+		std::cout << im << std::endl;
+		std::cout << m * im << std::endl;
 	}
 
 	std::cin.get();

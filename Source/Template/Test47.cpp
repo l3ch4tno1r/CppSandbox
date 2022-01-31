10a58b3fdc378fb71bc35a5b4f4ed4a59541e8bd
l3ch4tno1r <arnaud.granier34@gmail.com>
2022-01-31 20:11:31 +0100

    Test int to string literal

--- Source/Template/Test47.cpp
+++ Source/Template/Test47.cpp
@@ -36,7 +36,22 @@ int main()
 	std::cout << LCN::Literal<ContactVarName> << '\n';
 	std::cout << LCN::Literal<ContactVARName> << '\n';
 
-	constexpr size_t size = LCN::ToString<int, 505>::Size();
+	constexpr size_t size = LCN::ToString<size_t, 505>::Size();
+
+	std::cout << LCN::Literal<LCN::ToString<size_t,  505>>  << '\n';
+	std::cout << LCN::Literal<LCN::ToString<int,    -1505>> << '\n';
+	std::cout << LCN::Literal<LCN::ToString<int,    -2>>    << '\n';
+	std::cout << LCN::Literal<LCN::ToString<int,    -0>>    << '\n';
+	std::cout << LCN::Literal<LCN::ToString<int,    -153>>  << '\n';
+	std::cout << LCN::Literal<LCN::ToString<size_t,  26>>   << '\n';
+
+	using Contact0VarName = LCN::ConcatLiterals<ContactVarName, LCN::ToString<size_t, 0>>;
+	using Contact1VarName = LCN::ConcatLiterals<ContactVarName, LCN::ToString<size_t, 1>>;
+	using Contact2VarName = LCN::ConcatLiterals<ContactVarName, LCN::ToString<size_t, 2>>;
+
+	std::cout << LCN::Literal<Contact0VarName> << '\n';
+	std::cout << LCN::Literal<Contact1VarName> << '\n';
+	std::cout << LCN::Literal<Contact2VarName> << '\n';
 
 	std::cin.get();
 }

fa64b951d674c6aa9bb4955b0e70a4cd54ffad4c
l3ch4tno1r <arnaud.granier34@gmail.com>
2022-01-31 22:05:50 +0100

    Testing

--- Source/Template/Test47.cpp
+++ Source/Template/Test47.cpp
@@ -26,6 +26,16 @@ struct TableName
 struct Contact
 {};
 
+struct SelectLiteral : LCN::StringView<SelectLiteral>
+{
+	static constexpr std::string_view GetView() { return { "select" }; }
+};
+
+struct Separator : LCN::StringView<Separator>
+{
+	static constexpr std::string_view GetView() { return { ", " }; }
+};
+
 int main()
 {
 	using ContactTabName = TableName<Contact>;
@@ -35,8 +45,6 @@ int main()
 	std::cout << LCN::Literal<ContactTabName> << '\n';
 	std::cout << LCN::Literal<ContactVarName> << '\n';
 	std::cout << LCN::Literal<ContactVARName> << '\n';
-
-	constexpr size_t size = LCN::ToString<size_t, 505>::Size();
 
 	std::cout << LCN::Literal<LCN::ToString<size_t,  505>>  << '\n';
 	std::cout << LCN::Literal<LCN::ToString<int,    -1505>> << '\n';
@@ -52,6 +60,14 @@ int main()
 	std::cout << LCN::Literal<Contact0VarName> << '\n';
 	std::cout << LCN::Literal<Contact1VarName> << '\n';
 	std::cout << LCN::Literal<Contact2VarName> << '\n';
+
+	using TypeList   = LCN::TypeList<Contact0VarName, Contact1VarName, Contact2VarName>;
+	using ConcatType = LCN::ConcatLiterals<Contact0VarName, Contact1VarName, Contact2VarName>;
+
+	constexpr size_t size1 = ConcatType::Size();
+	constexpr size_t size2 = LCN::JoinLiteral<Separator, TypeList>::Size();
+
+	std::cout << LCN::Literal<LCN::JoinLiteral<Separator, TypeList>> << '\n';
 
 	std::cin.get();
 }
